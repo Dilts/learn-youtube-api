d74c06001df72f3c0ce182be86fabdef6783185c
Brian Dilts <brianedilts@gmail.com>
2021-04-21 14:34:23 -0600

    Initial Commit

---
+++ YouTube_API/YouTube_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouTube_APIApp.swift
+//  YouTube_API
+//
+//  Created by Brian Dilts on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct YouTube_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
